e3b948a6ac32a62fc32342692fefb66d90475245
Riya Johari <166673312+RiyaJ05@users.noreply.github.com>
2024-04-22 13:58:14 -0600

    adding get_available() method and modifying get_availability() and __str__() methods

--- book.py
+++ book.py
@@ -1,5 +1,3 @@
-
-
 class Book:
     GENRE_LIST = ["Romance", "Mystery", "Science Fiction", "Thriller", "Young Adult", "Children's Fiction", "Self-help", "Fantasy", "Historical Fiction", "Poetry"]
     
@@ -9,59 +7,58 @@ class Book:
         self.__isbn = isbn
         self.__genre = genre
         self.__availability = availability
-        return
     
     def get_title(self):
-        return self.__title
+      return self.__title
 
     def get_author(self):
-        return self.__author
+      return self.__author
     
     def get_isbn(self):
-        return self.__isbn
+      return self.__isbn
     
     def get_genre(self):
-        return self.__genre
+      return self.__genre
     
     def get_genre_name(self):
-        return Book.GENRE_LIST[self.__genre]
+      return Book.GENRE_LIST[self.__genre]
+
+    def get_available(self):
+      return self.__availability
     
     def get_availability(self):
-        return self.__availability
+      if self.__availability: 
+        return "Available"
+      else:
+        return "Borrowed"
 
     def borrow_it(self):
-        self.__availability = 'FALSE'
-        return
+      self.__available = False
 
     def return_it(self):
-        self.__availability = 'TRUE'
-        return
+      self.__available = True
 
     def set_isbn(self, new_isbn):
-        self.__isbn = new_isbn
-        return
+      self.__isbn = new_isbn
+      return
     
     def set_title(self, new_title):
-        self.__title = new_title
-        return
+      self.__title = new_title
+      return
     
     def set_author(self, new_author):
-        self.__author = new_author
-        return
+      self.__author = new_author
+      return
     
     def set_genre(self, new_genre):
-        self.__genre = new_genre
-        return
+      self.__genre = new_genre
+      return
 
     def __str__(self):
-        genre = self.get_genre_name()
-        if self.get_availability() == True: availability = "Available"
-        else: availability = "Borrowed"
-        isbn = self.get_isbn()
-        title = self.get_title()
-        author = self.get_author()
-        result = f'{isbn:<14s} {title:<25s} {author:<25s} {genre:<20s} {availability:<20s}'
-        return result
+      return f"{self.__isbn:<14s} {self.__title:<25s} {self.__author:<25s} {self.get_genre_name():<20s} {self.get_availability()}"
+
+
+
 
 
 
